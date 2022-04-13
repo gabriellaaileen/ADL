d867647e2cf5b496a565cfa171e7b039589df8ca
gabriellaaileen <99953890+gabriellaaileen@users.noreply.github.com>
2022-04-13 15:17:04 +0700

    Update PostProcessing.py

--- UTS ADL/PostProcessing.py
+++ UTS ADL/PostProcessing.py
@@ -56,8 +56,8 @@ class PostProcessing:
         """
         width, height = img.shape[1], img.shape[0]
         #process crop width and height for max available dimension
-        crop_width = dim[0] if dim[0]<img.shape[1] else img.shape[1]
-        crop_height = dim[1] if dim[1]<img.shape[0] else img.shape[0] 
+        crop_height = dim[0] if dim[0]<img.shape[1] else img.shape[1]
+        crop_width = dim[1] if dim[1]<img.shape[0] else img.shape[0] 
 
         mid_x, mid_y = int(width/2), int(height/2)
         cw2, ch2 = int(crop_width/2), int(crop_height/2) 
